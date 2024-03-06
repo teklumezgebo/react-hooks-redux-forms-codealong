ea5ce2d1138b83d43192728d06d37684d84f3cd9
Teklu Mezgebo <mezge002@umn.edu>
2024-03-06 12:32:25 -0600

    controlled form created to update state via dispatching reducer action

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { todoAdded } from "./todosSlice"
 
 function CreateTodo() {
-  return <div>Create Todo Component</div>;
+  
+  const dispatch = useDispatch()
+
+  const [text, setText] = useState("")
+
+  function handleChange(event) {
+    setText(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    dispatch(todoAdded(text))
+    setText("")
+  }
+  
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>add todo</label>
+          <input type="text" onChange={handleChange} value={text}/>
+        </p>
+        <input type="submit"/>
+      </form>
+    </div>
+  )
 }
 
 export default CreateTodo;
