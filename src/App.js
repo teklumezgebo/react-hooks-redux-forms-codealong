8e24d5d5c8a6167dbde051b4bd3ab18b046d0850
Teklu Mezgebo <mezge002@umn.edu>
2024-03-06 12:50:00 -0600

    useSelector hook to access state in ToDoContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CreateTodo from "./features/todos/CreateTodo";
+import TodoContainer from "./features/todos/TodoContainer";
 
 function App() {
   return (
     <div className="App">
       <CreateTodo />
+      <TodoContainer />
     </div>
   );
 }
